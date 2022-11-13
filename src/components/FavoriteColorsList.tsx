d3f51032c28b6f5e8ba5ce8c0d40477d2d05be82
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-13 18:07:03 +0100

    Added fake data list  and map the list with styles

--- src/components/FavoriteColorsList.tsx
+++ src/components/FavoriteColorsList.tsx
@@ -9,12 +9,14 @@ type FavoriteColorsListProps ={
 
 const FavoriteColorsList = (props:FavoriteColorsListProps) => {
   return (
-    <div>
+    <div className='my-favorite-color-list'>
+        <h2 className='my-favorite-colors-subtitle'>Color colection</h2>
         {props.colors.map(color=>{
             return(
-                <div>
-                    {color.name}
-                    {color.code}
+                <div className='fav-color-item' key={color.name}>
+                    <div  className='fav-color-code' style={{backgroundColor:`${color.code}`}}></div>
+                    <div className='fav-color-name'>{color.name}</div> 
+                    <button className='remove-color-button'>Remove color</button>
                 </div>
             )
         })}
