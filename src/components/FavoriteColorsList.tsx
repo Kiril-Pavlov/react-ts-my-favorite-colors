44c6d6f700558eba763fe236258918ebaab77b87
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-14 20:29:09 +0100

    no colors structure and styles done

--- src/components/FavoriteColorsList.tsx
+++ src/components/FavoriteColorsList.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
-interface FavoriteColorsListProps { 
-    colors:Color[]|[]
-}
+// import {SketchPicker} from "react-color"
 
-interface Color {
-    name: string; 
-    code:string;
-}
+import noColorsImg from "../assets/Vector.png"
 
+type FavoriteColorsListProps = {
+    colors:{
+        name: string,
+        code: string
+    }[]
+}
 
 const FavoriteColorsList = ({colors}:FavoriteColorsListProps) => {
   return (
     (colors.length !== 0 ? (
         <div className='my-favorite-color-list'>
             <h2 className='my-favorite-colors-subtitle'>Color colection</h2>
-            {colors.map((colors:Color)=>{
+            {colors.map((colors)=>{
                 return(
                     <div className='fav-color-item' key={colors.name}>
                         <div  className='fav-color-code' style={{backgroundColor:`${colors.code}`}}></div>
@@ -24,11 +25,20 @@ const FavoriteColorsList = ({colors}:FavoriteColorsListProps) => {
                     </div>
                 )
             })}
+            {/* <SketchPicker /> */}
         </div>
 
     ) : (
-        <div>
-            No colors
+        <div className='no-colors-container'>
+            <div className='no-colors-image-container'>
+                <img src={noColorsImg} alt="No Colors" />
+            </div>
+            <h2 className='no-colors-subtitle'>
+                Your color collection is empty!
+            </h2>
+            <p className='no-colors-paragraph'>
+                Your color collection is empty!
+            </p>
         </div>
     ))
   )
