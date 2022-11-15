0184567150628d16e84bf29e67031a10b7927b81
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-15 20:29:06 +0100

    local storage logic. small bug at getting data from LS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import FavoriteColorsList from './components/FavoriteColorsList';
 import SelectColor from './components/SelectColor';
@@ -20,9 +20,19 @@ function App() {
       code: "#00AA00"
     },
   ])
-  const [selectedColor, setSelectedColor] = useState<string>("")
+  const [selectedColor, setSelectedColor] = useState<string>("#78058f")
   console.log(selectedColor)
   const [customName, setCustomName] = useState<string>("")
+
+  //get data from localStorage after reloading the page
+  useEffect(() => {
+    if (localStorage.getItem("localColorsList")) {
+      //there is an error here in typescript if I dont add ||"[]" after getItem and it does not copy from local Storage
+      let localData = JSON.parse(localStorage.getItem("localColorslist")||"[]");
+      console.log(localData)
+      setMyFavoriteColors(localData)
+    }
+  }, [])
 
   //add color to my Favorite colors
   const addColor = () => {
@@ -33,7 +43,7 @@ function App() {
       }
       // console.log(taskList)
        setMyFavoriteColors([...myFavoriteColors, newColor]);
-      // localStorage.setItem("localTaskList", JSON.stringify([...taskList, newTask]))
+      localStorage.setItem("localColorsList", JSON.stringify([...myFavoriteColors, newColor]))
        setCustomName("")
       // console.log(taskList)
     }
@@ -44,7 +54,7 @@ function App() {
       
       const updated = myFavoriteColors.filter((c) => c.name !== color.name);
       setMyFavoriteColors(updated);
-      // localStorage.setItem("localColorList", JSON.stringify(updated))
+       localStorage.setItem("localColorsList", JSON.stringify(updated))
     }
 
   return (
